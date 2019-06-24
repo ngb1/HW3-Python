96137a23b1a286e12e665b04cc6283da1233ee41
51334657+ngb1@users.noreply.github.com <nbustamante2@outlook.com>
2019-06-23 23:05:01 -0500

    PyBank at 90%
    almost done, only missing how to obtain the first value of the change calculation automatically

--- PyBank/main.py
+++ PyBank/main.py
@@ -11,45 +11,76 @@ with open(csvpath, newline="") as csvfile:
     #counting number of months
     total_months = 0 
     total_PL = 0
+    #saving the first value on the list as old value... I haven't found the way to do this automatically without pandas library
+    # change_old is still a work in progress...
     change_old = 867884
+    change_oldmax = 0
+    change_oldmin = 0
     changes = [] 
+    #print(first_value)
     for rows in csvreader:
+    #counting the months, assuming there's one data value per month
         if str(rows[0]) != " ":
             total_months = total_months + 1 
     #summing up the profit/losses
         if str(rows[1]) != " ":
             total_PL = total_PL + float(rows[1])
+    #calculating the change
         if str(rows[1]) != " " and float(rows[1]) != change_old:
-            change = float(rows[1]) - change_old
-            # date_ch = float(rows[0]) 
-            # changes.append(date_ch)
+            change = float(rows[1]) - float(change_old)
+            # saving the change value if it's the maximum
+            if change > change_oldmax: 
+                change_max_new = change
+                date_max_new = str(rows[0])
+                change_oldmax = round(change_max_new)
+            # saving the change value if it's the minimum
+            if change < change_oldmin: 
+                change_min_new = change
+                date_min_new = str(rows[0])
+                change_oldmin = round(change_min_new)
+            # creating a new list for changes to calculate average
             changes.append(change)
+            # replacing old value by next row
             change_old = float(rows[1])
-            # A = enumerate(rows)
-            # for c in changes:
-            #     c = float(rows[1])*1
-            #     changes.append(enumerate(int(c)))
-        #length = len((rows[1]))
-        # total changes
+        # using the list changes to perform calculations
         total_changes = int(sum(changes))
         length = int(len(changes))
+    average_changes = round(total_changes / length, 2)
+    # the next two lines redundant but served to confirm the max incr... this was my first approach, but it didn't work becase
+    # it needed the reference dates
     max_incr = round(max(changes))
     min_decr = round(min(changes))
     total_months = round(total_months)
     total_PL = round(total_PL)
-    average_changes = round(total_changes / length, 2)
 
-
-
-        
+# print including formating        
 print("")
 print("--------------------------------------------------------------")
 print("Financial Analysis")
 print("--------------------------------------------------------------")
-print(f'Total Months:            {total_months}')
-print(f'Total Profit/Loss: {total_PL}')
-print(f'Average Change:       {average_changes}')
-print(f'Greatest Increase:  {max_incr}    @ DD/MM')
-print(f'Greatest Decrease: {min_decr}    @ DD/MM')
+print(f'Total Months:             {total_months}')
+print(f'Total Profit/Loss: ${total_PL}')
+print(f'Average Change:       ${average_changes}')
+print(f'Greatest Increase:  ${max_incr}    @ {date_max_new}')
+print(f'Greatest Decrease: ${min_decr}    @ {date_min_new}')
 print("")
 print("--------------------------------------------------------------")
+
+# creating file
+print("Creating file with Financial Analysis...")
+
+# I modified the code from https://www.guru99.com/reading-and-writing-files-in-python.html
+f= open("Financial_Analysis.txt","w+")
+f.write("Financial Analysis\n") 
+f.write("--------------------------------------------------------------\n")
+f.write(f'Total Months:             {total_months}\n')
+f.write(f'Total Profit/Loss: ${total_PL}\n')
+f.write(f'Average Change:       ${average_changes}\n')
+f.write(f'Greatest Increase:  ${max_incr}    @ {date_max_new}\n')
+f.write(f'Greatest Decrease: ${min_decr}    @ {date_min_new}\n')
+f.write("\n")
+f.write("--------------------------------------------------------------\n")
+f.close()
+
+# creating file
+print("File created successfully")
