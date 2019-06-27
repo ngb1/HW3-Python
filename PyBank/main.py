492dd7a6001e63a66b33969285eb577a860ba5c3
51334657+ngb1@users.noreply.github.com <nbustamante2@outlook.com>
2019-06-26 23:54:45 -0500

    Update main.py
    100%

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,7 +1,7 @@
 import os
 import csv
 
-csvpath = os.path.join("budget_data.csv")
+csvpath = os.path.join("..","..","..","RICEHOU201906DATA1","HW","03-Python","Instructions", "PyBank", "Resources","budget_data.csv")
 
 with open(csvpath, newline="") as csvfile:
     # Split the data on commas
@@ -13,21 +13,30 @@ with open(csvpath, newline="") as csvfile:
     total_PL = 0
     #saving the first value on the list as old value... I haven't found the way to do this automatically without pandas library
     # change_old is still a work in progress...
-    change_old = 867884
+    # I obtained figured I could convert the csv column into a list, then read the first value with indexing
     change_oldmax = 0
     change_oldmin = 0
     changes = [] 
-    #print(first_value)
+    pl = 0
+    pl_list = []
+    change_old = 0 #867884
+
+    # I could not use the first value of the list, I had to use pop(0) instead
+    # for r in csvreader:
+    #     pl = r[1]
+    #     pl_list.append(pl)
+    # change_old = 0 #pl_list[0]
+        #print(change_old)
     for rows in csvreader:
     #counting the months, assuming there's one data value per month
         if str(rows[0]) != " ":
             total_months = total_months + 1 
     #summing up the profit/losses
-        if str(rows[1]) != " ":
+        if int(rows[1]) != " ":
             total_PL = total_PL + float(rows[1])
     #calculating the change
-        if str(rows[1]) != " " and float(rows[1]) != change_old:
-            change = float(rows[1]) - float(change_old)
+        if int(rows[1]) != 0:
+            change = float(rows[1]) - change_old
             # saving the change value if it's the maximum
             if change > change_oldmax: 
                 change_max_new = change
@@ -40,19 +49,29 @@ with open(csvpath, newline="") as csvfile:
                 change_oldmin = round(change_min_new)
             # creating a new list for changes to calculate average
             changes.append(change)
+            
             # replacing old value by next row
             change_old = float(rows[1])
+
+            #average_changes = round(total_changes / length, 2)
         # using the list changes to perform calculations
-        total_changes = int(sum(changes))
-        length = int(len(changes))
-    average_changes = round(total_changes / length, 2)
+        #print(changes.pop(0))
+        #changes.pop(0)
+    changes.pop(0)
+    length = len(changes) 
+    total_changes = int(sum(changes))
+    
+    #print(length)
+    #print(total_changes)
     # the next two lines redundant but served to confirm the max incr... this was my first approach, but it didn't work becase
+    changes.pop(0)
+    average_changes = round(total_changes / length, 2)
     # it needed the reference dates
     max_incr = round(max(changes))
     min_decr = round(min(changes))
     total_months = round(total_months)
     total_PL = round(total_PL)
-
+#changes.pop(0)
 # print including formating        
 print("")
 print("--------------------------------------------------------------")
