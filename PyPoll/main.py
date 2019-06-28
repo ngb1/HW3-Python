cbb483b74c7ad5319640c64e55f85294af08f6a5
51334657+ngb1@users.noreply.github.com <nbustamante2@outlook.com>
2019-06-28 17:14:45 -0500

    PyPoll and PyBank completed
    PyPoll and PyBank completed

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -14,6 +14,7 @@ with open(csvpath, newline="") as csvfile:
     vote = 0
     candidate_list = []
     vote_count = {}
+    percent_count = {}
     vote_list = []
     for rows in csvreader:
         voter = rows[0]
@@ -27,6 +28,7 @@ with open(csvpath, newline="") as csvfile:
         #option 1: updating the voter count per candidate using candidate as index in the vote_count dictionary
         if candidate in vote_count:
             vote_count[candidate] = vote_count[candidate] + 1
+            percent_count[candidate] = round(vote_count[candidate] * 100 / total_voters,3)
         else:
             vote_count[candidate] = 1
         #option 2: creating a list from the candidates list
@@ -34,14 +36,18 @@ with open(csvpath, newline="") as csvfile:
             #updating the candidates list
             candidate_list.append(candidate)
             vote_list.append(1)
-        for c in candidates:
+        #for c in candidates:
         #     vote_count.append(vote)
         #     if candidate == c:
         #         vote = vote + 1 #in vote_count
-    print(vote_count)        
-    print(total_voters)
-    print(candidate_list)
-    print(len(candidates))
+    #Winner: I obtained this code from thie site: https://stackoverflow.com/questions/42044090/return-the-maximum-value-from-a-dictionary
+    max_value = max(vote_count.values())  # maximum value
+    max_keys = [k for k, v in vote_count.items() if v == max_value] # getting all keys containing the `maximum`
+    
+    # print(vote_count)        
+    # print(total_voters)
+    # print(candidate_list)
+    # print(len(candidates))
     # for c_ref in range(len(candidates)):
     #     vote_count.append(vote)
     #     vote = 0
@@ -98,11 +104,37 @@ with open(csvpath, newline="") as csvfile:
     # #         c1_votes = c1_votes + 1 
 
     # # c1_percent = round(c1_votes / total_voters * 100, 3)
-    # print("")
-    # print("Election Results")
-    # print("----------------------------------------------------")
-    # print(f'Total votes:  {total_voters}')
-    # print("----------------------------------------------------")
-    # print(f'Candidates:  {candidates}')
-    # print("----------------------------------------------------")
-    # # print(f'Khan:  {c1_percent}% ({c1_votes})')
+print("")
+print("Election Results")
+print("--------------------------------------------------------------------------------------------------------")
+print(f'Total votes:  {total_voters}')
+print("--------------------------------------------------------------------------------------------------------")
+print(f'Candidates:  {candidate_list}')
+print(f'Votes:       {vote_count}')
+print(f'Percent (%): {percent_count}')
+print("")
+print("--------------------------------------------------------------------------------------------------------")
+print(f'Winner:      {max_keys}')
+print("")
+print("--------------------------------------------------------------------------------------------------------")
+# # print(f'Khan:  {c1_percent}% ({c1_votes})')
+
+
+# creating file
+print("Creating file with Election Results...")
+
+# I modified the code from https://www.guru99.com/reading-and-writing-files-in-python.html
+f= open("Election_Results.txt","w+")
+f.write("Election Results\n") 
+f.write("----------------------------------------------------------------------------------------------------------------------------\n")
+f.write(f'Total votes:  {total_voters}\n')
+f.write(f'Candidates:  {candidate_list}\n')
+f.write(f'Votes:       {vote_count}\n')
+f.write(f'Percent (%): {percent_count}\n')
+f.write("\n")
+f.write(f'Winner:      {max_keys}\n')
+f.write("----------------------------------------------------------------------------------------------------------------------------\n")
+f.close()
+
+# creating file
+print("File created successfully")
